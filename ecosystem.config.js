5f6541451eafc446d490ab1e1b8fb3e302c1e0b1
Rizal <edith@Rizals-MacBook-Air.local>
2024-08-16 09:59:45 +0700

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "root",
+      cwd: "/Users/edith/Documents/Project/Ajinomoto/memberarea/mf-company-root",
+      script: "npm",
+      args: "start",
+    },
+  ],
+};
